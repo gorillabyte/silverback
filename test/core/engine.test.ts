eaab9396ef2b69e848006fa5c6519f095e9e603c
Stefan Herndlbauer <sherndlbauer@gorillabyte.com>
2015-12-19 20:15:13 +0100

    Add more coverage to the first test

--- test/core/engine.test.ts
+++ test/core/engine.test.ts
@@ -1,9 +1,12 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
 import {Engine} from '../../src/core/Engine';
+import {System} from '../../src/core/System';
 
 describe('First test', function() {
     it('should create an instance of silverback engine', function() {
         var e = new Engine();
+        e.addSystem(new System(), 0);
+        e.removeAllSystems();
     });
 });
