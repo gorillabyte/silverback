ee39c9758e74377d4eb2902c6590423dab3e0c5a
Stefan Herndlbauer <sherndlbauer@gorillabyte.com>
2015-12-31 12:34:40 +0200

    Add sorting function for system priorities

---
+++ src/utils/SystemSort.ts
@@ -0,0 +1,96 @@
+/*
+ * Insertion sort implementation in JavaScript
+ * Copyright (c) 2012 Nicholas C. Zakas
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of items software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and items permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * Swaps two values in an array.
+ * @param {Array} items The array containing the items.
+ * @param {int} firstIndex Index of first item to swap.
+ * @param {int} secondIndex Index of second item to swap.
+ * @return {void}
+ */
+function swap(items, firstIndex:number, secondIndex:number):void {
+    let temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
+}
+
+function partition(items, left:number, right:number) {
+    let pivot = items[Math.floor((right + left) / 2)].priority;  // pivot value is middle item
+    let i = left;     // starts from left and goes right to pivot index
+    let j = right;    // starts from right and goes left to pivot index
+
+    // while the two indices don't match
+    while (i <= j) {
+
+        // if the item on the left is less than the pivot, continue right
+        while (items[i].priority < pivot) {
+            i++;
+        }
+
+        // if the item on the right is greater than the pivot, continue left
+        while (items[j].priority > pivot) {
+            j--;
+        }
+
+        // if the two indices still don't match, swap the values
+        if (i <= j) {
+            swap(items, i, j);
+
+            // change indices to continue loop
+            i++;
+            j--;
+        }
+    }
+    // this value is necessary for recursion
+    return i;
+}
+
+/**
+ * A quicksort implementation in JavaScript. The array is sorted in place.
+ * @param {Array} items An array of items to sort.
+ * @return {Array} The sorted array.
+ */
+export default function systemSort(items, left?, right?) {
+    let index;
+
+    // performance - don't sort an array with zero or one items
+    if (items.length > 1) {
+
+        // fix left and right values - might not be provided
+        left = typeof left !== 'number' ? 0 : left;
+        right = typeof right !== 'number' ? items.length - 1 : right;
+
+        // split up the entire array
+        index = partition(items, left, right);
+
+        // if the returned index
+        if (left < index - 1) {
+            systemSort(items, left, index - 1);
+        }
+
+        if (index < right) {
+            systemSort(items, index, right);
+        }
+    }
+    return items;
+}
