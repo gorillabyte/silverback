f958e370e495fd24f5a2c7970e448226c9b2ce30
Stefan Herndlbauer <sherndlbauer@gorillabyte.com>
2016-07-30 16:43:57 +0200

    Remove the duplicated webpack config.

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,7 @@
-module.exports = function(config) {
-    config.set({
+const webpackConfig = require('./webpack.config.js');
 
+module.exports = function setKarmaConfig(config) {
+    config.set({
         basePath: '',
         frameworks: ['mocha', 'chai'],
         files: [
@@ -27,39 +28,8 @@ module.exports = function(config) {
                 ui: 'bdd'
             }
         },
-        webpack: {
-            resolve: {
-                cache: false,
-                root: __dirname,
-                extensions: ['','.ts','.js']
-            },
-            devtool: 'source-map',
-            module: {
-                loaders: [{
-                    test: /\.ts(x?)$/,
-                    exclude: /node_modules/,
-                    loader: 'babel-loader?presets[]=es2015!ts-loader'
-                }, {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    loader: 'babel',
-                    query: {
-                        presets: ['es2015']
-                    }
-                }],
-                postLoaders: [
-                    {
-                        test: /\.(js|ts)$/,
-                        loader: 'istanbul-instrumenter-loader',
-                        exclude: [ /(test|node_modules)\// ]
-                    }
-                ]
-            },
-            stats: { colors: true, reasons: true },
-            debug: false
-        },
+        webpack: webpackConfig,
         webpackMiddleware: { noInfo: true },
-
         coverageReporter: {
             dir: './coverage/',
             reporters:[
@@ -69,7 +39,6 @@ module.exports = function(config) {
                 {type: 'text-summary', subdir: '.', file: 'text-summary.txt'}
             ]
         },
-
         colors: true
     });
 };
